895a6171c2c8e2bc375decb9c11ffa4051b48f83
Tianyi.Zhan <tianyi.zhan@outlook.com>
2022-02-15 10:42:17 +0800

    initialize project

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:taskmanager/screens/homeScreen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const homeScreen(), // initial route
+    );
+  }
+}
+
